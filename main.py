63ef024d39c28b6dc6fbaf7dba2f3294236bb824
wobertt <robertchung1462@gmail.com>
2023-04-24 15:42:34 -0400

    Create settings page

--- main.py
+++ main.py
@@ -10,5 +10,11 @@ def game():
     return render_template('game.html')
 
 
+@app.route('/settings', methods=['GET', 'POST'])
+def settings():
+    if request.method == 'GET':
+        return render_template('settings.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81, debug=True)
